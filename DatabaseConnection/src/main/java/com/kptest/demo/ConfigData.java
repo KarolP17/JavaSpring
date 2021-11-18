c14d12b4bd21d31127f623d59c91cfd7dbcd1c9e
KarolP17 <93950629+KarolP17@users.noreply.github.com>
2021-11-18 14:17:43 +0100

    Add files via upload

---
+++ DatabaseConnection/src/main/java/com/kptest/demo/ConfigData.java
@@ -0,0 +1,41 @@
+package com.kptest.demo;
+
+public class ConfigData {
+    private int range;
+    private int start;
+    private int end;
+    public ConfigData(int range, int start, int end){
+        this.range = range;
+        this.start = start;
+        this.end = end;
+    }
+
+    public ConfigData() {
+
+    }
+
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+}
